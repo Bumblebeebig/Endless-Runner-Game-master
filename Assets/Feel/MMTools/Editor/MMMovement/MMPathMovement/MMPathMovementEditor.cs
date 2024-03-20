86db3c8788b630f126f90e00953bcf559d6c9bf0
Bumblebeebig <100919536+Bumblebeebig@users.noreply.github.com>
2024-03-20 17:10:29 +0700

    add mobile tool

--- Assets/Feel/MMTools/Editor/MMMovement/MMPathMovement/MMPathMovementEditor.cs
+++ Assets/Feel/MMTools/Editor/MMMovement/MMPathMovement/MMPathMovementEditor.cs
@@ -62,7 +62,7 @@ namespace MoreMountains.Tools
 				Handles.Label(t.PointPosition(i) + (Vector3.down*0.4f) + (Vector3.right*0.4f), ""+i,style);
 
 				// draws a movable handle
-				var fmh_65_57_638465286136564124 = Quaternion.identity; Vector3 newPoint = Handles.FreeMoveHandle(oldPoint,.5f,new Vector3(.25f,.25f,.25f),Handles.CircleHandleCap);
+				var fmh_65_57_638465388452380982 = Quaternion.identity; Vector3 newPoint = Handles.FreeMoveHandle(oldPoint,.5f,new Vector3(.25f,.25f,.25f),Handles.CircleHandleCap);
 
 				// records changes
 				if (EditorGUI.EndChangeCheck())
